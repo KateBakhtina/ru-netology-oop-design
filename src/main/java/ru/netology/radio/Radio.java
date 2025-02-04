76d210cf3cdf3af8d25c1d15769737ac3c4f4e90
Kate Bakhtina <almazniegori@gmail.com>
2025-02-04 23:14:48 +0300

    initial_commit

---
+++ src/main/java/ru/netology/radio/Radio.java
@@ -0,0 +1,56 @@
+package ru.netology.radio;
+
+public class Radio {
+
+    private int currentStation;
+    private int currentVolume;
+
+    public void setCurrentStation(int newStation) {
+        if (newStation >= 0 && newStation <= 9) {
+            currentStation = newStation;
+        }
+    };
+
+    public int getCurrentStation() {
+        return currentStation;
+    };
+
+    public void next() {
+        if (currentStation == 9) {
+            currentStation = 0;
+        } else {
+            currentStation ++;
+        }
+    };
+
+    public void prev() {
+        if (currentStation == 0) {
+            currentStation = 9;
+        } else {
+            currentStation --;
+        }
+     };
+
+    public void setCurrentVolume(int newVolume) {
+        if (newVolume >= 0 && newVolume <= 100) {
+            currentVolume = newVolume;
+        }
+    };
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    };
+
+    public int turnUpVolume() {
+        if (currentVolume < 100) {
+            currentVolume ++;
+        }
+        return currentVolume;
+    };
+
+    public void turnDownVolume() {
+        if (currentVolume > 0) {
+            currentVolume --;
+        }
+    };
+}
---
+++ src/test/java/ru/netology/radio/RadioTest.java
@@ -0,0 +1,79 @@
+package ru.netology.radio;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+public class RadioTest {
+
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/dataForCurrentStationTest.csv")
+    public void CurrentStationTest(int newStation, int expectedStation) {
+        Radio station = new Radio();
+
+        station.setCurrentStation(newStation);
+        int actualStation = station.getCurrentStation();
+
+        Assertions.assertEquals(actualStation, expectedStation);
+    };
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/dataForNextTest.csv")
+    public void nextTest(int newStation, int expected) {
+        Radio station = new Radio();
+
+        station.setCurrentStation(newStation);
+        station.next();
+        int actual = station.getCurrentStation();
+
+        Assertions.assertEquals(actual, expected);
+    };
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/dataForPrevTest.csv")
+    public void prevTest(int newStation, int expected) {
+        Radio station = new Radio();
+
+        station.setCurrentStation(newStation);
+        station.prev();
+        int actual = station.getCurrentStation();
+
+        Assertions.assertEquals(actual, expected);
+    };
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/dataForCurrentVolumeTest.csv")
+    public void currentVolumeTest(int newVolume, int expected) {
+        Radio station = new Radio();
+
+        station.setCurrentVolume(newVolume);
+        int actual = station.getCurrentVolume();
+
+        Assertions.assertEquals(actual, expected);
+    };
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/dataForTurnUpVolumeTest.csv")
+    public void turnUpVolumeTest(int newVolume, int expected) {
+        Radio station = new Radio();
+
+        station.setCurrentVolume(newVolume);
+        station.turnUpVolume();
+        int actual = station.getCurrentVolume();
+
+        Assertions.assertEquals(actual, expected);
+    };
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/dataForTurnDownVolumeTest.csv")
+    public void turnDownVolumeTest(int newVolume, int expected) {
+        Radio station = new Radio();
+
+        station.setCurrentVolume(newVolume);
+        station.turnDownVolume();
+        int actual = station.getCurrentVolume();
+
+        Assertions.assertEquals(actual, expected);
+    }
+}
